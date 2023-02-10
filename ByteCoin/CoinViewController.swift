21117c44bfd0b0cd3873aa9911dfe4d64b73ec54
Bektemur Mamashaev <123092077+bb-pro@users.noreply.github.com>
2023-02-10 12:23:58 +0500

    add extensions

--- ByteCoin/CoinViewController.swift
+++ ByteCoin/CoinViewController.swift
@@ -7,29 +7,31 @@
 
 import UIKit
 
-class CoinViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, CoinManagerDelegate {
+//MARK: - CoinViewController
+class CoinViewController: UIViewController, UIPickerViewDataSource {
     
-  
-    
-    
+    //MARK: - IB Outlets
     @IBOutlet var rateLabel: UILabel!
     @IBOutlet var currencyLabel: UILabel!
     @IBOutlet var currencyPicker: UIPickerView!
     @IBOutlet var btcImageView: UIImageView!
     
-    var coinManager = CoinManager()
+    //MARK: - Private Properties
+    private var coinManager = CoinManager()
     
+    //MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+
         coinManager.delegate = self
-        
         currencyPicker.delegate = self
         currencyPicker.dataSource = self
     }
-    
-    
-    
-    
+}
+
+
+//MARK: - CoinManagerDelegate
+extension CoinViewController: CoinManagerDelegate {
     func didUpdateWeather(_ coinManager: CoinManager, coin: CoinModel) {
         DispatchQueue.main.async {
             self.rateLabel.text = String(format: "%.3f", coin.rate)
@@ -37,27 +39,28 @@ class CoinViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
             self.btcImageView.isHidden = false
         }
     }
-    
     func didFailWithError(error: Error) {
         print("Error")
     }
-    
-    
-    
+}
+
+//MARK: - UIPickerViewDelegate
+extension CoinViewController: UIPickerViewDelegate {
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
+    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return coinManager.currencyArray.count
     }
+    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return coinManager.currencyArray[row]
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         let selectedCurrency = coinManager.currencyArray[row]
         coinManager.getCoinPrice(for: selectedCurrency)
-        
     }
 }
 
