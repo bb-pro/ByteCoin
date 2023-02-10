7221e7ce3f1cffa01e977a4a4764b3e8208a3cf8
Bektemur Mamashaev <123092077+bb-pro@users.noreply.github.com>
2023-02-10 10:03:58 +0500

    Initial Commit

---
+++ ByteCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Bektemur on 10/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
